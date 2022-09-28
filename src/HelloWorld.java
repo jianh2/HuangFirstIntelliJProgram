7c43275c5b3f968d69a7fd160d41a22f899ffc8b
jianh2 <Jianh2@Nycstudents.net>
2022-09-28 08:35:06 -0400

    Added a print statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.println("Today is September 29th");
+    }
+
+
 }
